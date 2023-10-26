90c82ff07e4e8b6e65ea23c10f3980d3594e8237
mayy58 <mayrampito@gmail.com>
2023-10-26 19:20:35 -0300

    quiz-page

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -18,6 +18,7 @@ class _QuizPageState extends State<QuizPage> {
           elevation: 0,
         ),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Container(
               margin: const EdgeInsets.symmetric(horizontal: 30),
@@ -30,7 +31,43 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
             ),
-            Container(),
+            ConstrainedBox(
+              constraints: const BoxConstraints(maxHeight: 450),
+              child: Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+                  child: Card(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.all(15),
+                          child: const Text("Pregunta 1"),
+                        ),
+                        Flexible(
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: 5,
+                            itemBuilder: (_, index) {
+                              return Container(
+                                margin: const EdgeInsets.all(3),
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Colors.indigo.shade100,
+                                        width: 2)),
+                                child: ListTile(
+                                  leading: Text("${index + 1}"),
+                                  title: Text("Respuesta"),
+                                  onTap: () {},
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  )),
+            ),
             TextButton(
               onPressed: () {},
               child: const Text("Skip"),
